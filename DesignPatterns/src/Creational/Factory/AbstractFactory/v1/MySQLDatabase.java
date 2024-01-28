337ccda6ab108bfe9a294bd707fad27c5570ec90
Tanuj <itsmetanuj4u@gmail.com>
2024-01-28 22:36:05 +0530

    Factory Design Pattern logic fix

--- DesignPatterns/src/Creational/Factory/AbstractFactory/v1/MySQLDatabase.java
+++ DesignPatterns/src/Creational/Factory/AbstractFactory/v1/MySQLDatabase.java
@@ -1,4 +1,8 @@
 package Creational.Factory.AbstractFactory.v1;
 
 public class MySQLDatabase implements Database {
+    @Override
+    public DatabaseFactory createDatabaseFactory() {
+        return new MySQLDBFactory();
+    }
 }
--- DesignPatterns/src/Creational/Factory/AbstractFactory/v1/PostgreSQLDatabase.java
+++ DesignPatterns/src/Creational/Factory/AbstractFactory/v1/PostgreSQLDatabase.java
@@ -1,4 +1,8 @@
 package Creational.Factory.AbstractFactory.v1;
 
 public class PostgreSQLDatabase implements Database {
+    @Override
+    public DatabaseFactory createDatabaseFactory() {
+        return new PostgreSQLDBFactory();
+    }
 }
