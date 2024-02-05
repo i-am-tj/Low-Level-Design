92586f705b0dfa0346c374c24b44bc476dd2dbdc
Tanuj <itsmetanuj4u@gmail.com>
2024-02-05 19:09:57 +0530

    TTT - Included MediumBotPlayingStrategy

--- Problems/TicTacToe/src/strategies/botplayingstrategies/MediumBotPlayingStrategy.java
+++ Problems/TicTacToe/src/strategies/botplayingstrategies/MediumBotPlayingStrategy.java
@@ -2,10 +2,23 @@ package strategies.botplayingstrategies;
 
 import models.Board;
 import models.Cell;
+import models.CellStatus;
+
+import java.util.List;
+import java.util.Random;
 
 public class MediumBotPlayingStrategy implements BotPlayingStrategy{
     @Override
     public Cell makeMove(Board board) {
-        return null;
+        int size = board.getSize();
+        Random r = new Random();
+        int col = r.nextInt(0, size);
+        int row = r.nextInt(0, size);
+        List<List<Cell>> curr = board.getBoard();
+        while(!curr.get(row).get(col).getCellStatus().equals(CellStatus.EMPTY)) {
+            col = r.nextInt(0, size);
+            row = r.nextInt(0, size);
+        }
+        return new Cell(row, col);
     }
 }
